84e10992e6d4e17551a54d0182f5da97d77b6603
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2026-05-21 00:06:56 -0600

    Finished AuthContext
    -AuthContext now fetches "logout" and "refresh" endpoints

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -8,23 +8,59 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [authLoading, setAuthLoading] = useState(true);
 
+  const logoutUser = async () => {
+    try {
+      await fetch(`${API_URL}/api/auth/logout`, {
+        method: "POST",
+        credentials: "include"
+      });
+    } catch {}
+
+    setUser(null);
+  };
+
   useEffect(() => {
     const checkAuth = async () => {
       try{
-        const res = await fetch (`${API_URL}/api/auth/me`, {
+        // Authenticate user
+        const resMe = await fetch (`${API_URL}/api/auth/me`, {
           credentials: "include"
         });
 
-        const data = await res.json();
+        const dataMe = await resMe.json();
 
-        if (!res.ok){
-          setUser(null);
+        if (!resMe.ok){
+          //Refresh access token
+          const resRefresh = await fetch(`${API_URL}/api/auth/refresh`, {
+            method: "POST",
+            credentials: "include"
+          });
+
+          if(!resRefresh.ok){
+            // Log out user
+            await logoutUser();
+            return;
+          }
+
+          // Authenticate user again
+          const resMeVerification = await fetch (`${API_URL}/api/auth/me`, {
+            credentials: "include"
+          });
+
+          const dataMeVerification = await resMeVerification.json();
+          
+          if(!resMeVerification.ok){
+            // Log out user
+            await logoutUser();
+            return;
+          }
+
+          setUser(dataMeVerification.user);
           return;
-
         }
 
         // Set user
-        setUser(data.user);
+        setUser(dataMe.user);
       } catch (error) {
         setUser(null);
       } finally {
